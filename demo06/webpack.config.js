3932644addeb7c17dfb3636ec6dc99e1c0e6add9
zhoujinjina <3513776811@qq.com>
2023-08-01 21:38:05 +0800

    update demo06

---
+++ demo06/webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: './main.jsx',
+    output: {
+      filename: 'bundle.js'
+    },
+    module:{
+      rules:[
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use:{
+            loader: 'babel-loader',
+            options:{
+              presets:['es2015', 'react']
+            }
+          }
+        },
+        {
+          test:/\.css$/,
+          use:[
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options:{
+                module:true
+              }
+            }
+          ]
+        }
+      ]
+    }
+  };
